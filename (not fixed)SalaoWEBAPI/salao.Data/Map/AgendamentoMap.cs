d0a5d4971efb68e2012afeca08824d86bdec3f61
victor paglione <79753033+vicpaglione@users.noreply.github.com>
2021-03-13 18:15:06 -0300

    Add files via upload

---
+++ (not fixed)SalaoWEBAPI/salao.Data/Map/AgendamentoMap.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace salao.Data.Map
+{
+    public class AgendamentoMap : : IEntityTypeConfiguration<Agendamento>
+    {
+         public void Configure(EntityTypeBuilder<Agendamento> builder)
+    {
+        builder.ToTable("Agendamento");
+
+        builder.HasKey(x => x.Id);
+
+        builder.HasOne<Cliente>(a => a.Cliente).WithMany(c => c.Agendamentos);
+
+        builder.HasOne<ServicoSolicitado>(a => a.ServicoSolicitado)
+            .WithMany(ss => ss.Agendamentos);
+
+        builder.Property(a => a.DataAgendamento).HasColumnType("datetime");
+
+        builder.Property(x => x.Anotacao).HasColumnType("varchar(100)");
+
+        builder.Property(x => x.Status).HasColumnType("int")
+            .IsRequired();
+
+        builder.HasOne<Pagamento>(a => a.Pagamento)
+            .WithOne(p => p.AgendamentoRealizado);
+    }
+}
---
+++ (not fixed)SalaoWEBAPI/salao.Data/Map/ClienteMap.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace salao.Data.Map
+{
+    class ClienteMap
+    {
+        public class ClienteMap : IEntityTypeConfiguration<Cliente>
+        {
+            public void Configure(EntityTypeBuilder<Cliente> builder)
+            {
+                builder.ToTable("Cliente");
+
+                builder.HasKey(x => x.Id);
+
+                builder.Property(x => x.Nome).HasColumnType("varchar(150)")
+                    .IsRequired();
+
+                builder.Property(x => x.Telefone).HasColumnType("varchar(15)")
+                   .IsRequired();
+
+                builder.Property(x => x.Cpf).HasColumnType("varchar(11)")
+                   .IsRequired();
+            }
+        }
+    }
+}
